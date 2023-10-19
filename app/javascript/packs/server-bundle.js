2cdb2597880ef1fb2335df33023e1fd61b49edb4
arthur.jacquemin <arthurjacquemin2@gmail.com>
2023-10-19 15:07:30 +0200

    feat: Notification component

--- app/javascript/packs/server-bundle.js
+++ app/javascript/packs/server-bundle.js
@@ -1,8 +1,10 @@
 import ReactOnRails from 'react-on-rails';
 
 import HelloWorld from '../bundles/HelloWorld/components/HelloWorldServer';
+import Notification from "../bundles/Notification/components/Notification";
 
 // This is how react_on_rails can see the HelloWorld in the browser.
 ReactOnRails.register({
   HelloWorld,
+  Notification
 });
