e47f14665ed0df8deb3ec37810a661027ef3e972
arthur.jacquemin <arthurjacquemin2@gmail.com>
2023-10-30 15:17:28 +0100

    refactor: wines_controller using wine_helper

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -3,6 +3,8 @@
 # Wines controller
 class WinesController < ApplicationController
   include Pagy::Backend
+  include WineHelper
+
   before_action :set_wine, only: %i[show]
 
   def index
@@ -25,45 +27,7 @@ class WinesController < ApplicationController
 
   private
 
-  def search_wines
-    if params[:q].present?
-      save_search
-      search_response = Wine.__elasticsearch__.search(
-        query: {
-          multi_match: {
-            query: params[:q],
-            fields: %w[name properties marketplace]
-          },
-        },
-        sort: [{ note: 'desc' }]
-      )
-      searches = search_response.records
-      pagy(searches, items: 6)
-    else
-      pagy(sorted_wines, items: 6)
-    end
-  end
-
-  def save_search
-    return unless user_signed_in? || expert_signed_in?
-
-    search = Search.find_or_create_by!(query: params[:q]) do |s|
-      s.expert_id = current_expert&.id
-      s.user_id = current_user&.id
-    end
-
-    search.touch
-  end
-
-  def redirect_to_last_page
-    redirect_to action: 'search', q: params[:q]
-  end
-
   def set_wine
     @wine = Wine.find(params[:id])
-  end
-
-  def sorted_wines
-    Wine.order(note: :desc)
   end
 end
